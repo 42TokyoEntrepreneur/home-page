aabfe5938b71de82e89ff95b04337f0825c4ecf3
WakamiyaYuma <root1@WakamiyaYumanoMacBook-Pro.local>
2024-11-16 16:34:53 +0900

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./layouts/**/*.html",
+    "./content/**/*.md",
+    "./themes/startup-knowledge-theme/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
